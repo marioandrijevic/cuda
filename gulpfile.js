47d53feb2b42d3efcc53a85d89d8075abc43d1d4
marioandrijevic <aandrijevicc@gmail.com>
2023-04-16 06:38:45 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+
+const { series } = require('gulp');
+const { parallel } = require('gulp');
+
+// Compile SCSS into CSS
+function scss() {
+    return gulp
+        .src('app/styles/scss/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: true,
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('app/styles/.'));
+}
+
+// Minify JS file
+function js() {
+    return gulp
+        .src(['src/scripts/*.js'])
+        .pipe(plumber())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/'))
+}
+
+
+
+async function watch() {
+    gulp.watch('app/styles/scss/**/*.scss', gulp.series(scss));
+}
+
+exports.scss = scss;
+exports.js = js;
+exports.default = watch;
